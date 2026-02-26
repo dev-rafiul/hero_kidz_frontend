91960e51e9961e8d82ec67b790bf1af0db8ffd15
COMRADE040 <rafiulislam040@gmail.com>
2026-02-26 23:10:53 +0600

    remove external ui

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-       <div className="navbar bg-base-100 shadow-sm">
+       <div className="navbar bg-base-100 ">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -13,11 +13,7 @@ const Navbar = () => {
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
         <li><a>Item 1</a></li>
         <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
+          <a href="">Item 2</a>
         </li>
         <li><a>Item 3</a></li>
       </ul>
@@ -28,13 +24,9 @@ const Navbar = () => {
     <ul className="menu menu-horizontal px-1">
       <li><a>Item 1</a></li>
       <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2 bg-base-100 w-40 z-1">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
+        <a>
+       Item 3
+        </a>
       </li>
       <li><a>Item 3</a></li>
     </ul>
