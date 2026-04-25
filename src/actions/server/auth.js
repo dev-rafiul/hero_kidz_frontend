dbd8c4b7e86ce57650f69c9f2c53a2d691a60018
COMRADE040 <rafiulislam040@gmail.com>
2026-04-25 20:28:24 +0600

    fix auth redirections session&token

--- src/actions/server/auth.js
+++ src/actions/server/auth.js
@@ -1,64 +1,113 @@
-"use server"
+// "use server"
 
-import { collections, dbConnect } from "@/lib/dbConnect"
-import bcrypt from "bcryptjs"
+// import { collections, dbConnect } from "@/lib/dbConnect"
+// import bcrypt from "bcryptjs"
+
+// export const postUser = async (payload) => {
+
+
+
+//     const {email, password, name} = payload
+
+
+//     if(!email || !password ) return null
+
+
+//     const isExist = await dbConnect(collections.USERS).findOne({email})
+//     if(isExist){
+//         return null
+//     }
+
+
+
+//     const newUser = {
+//         providerId: "credentials",
+//         name, email, password: await bcrypt.hash(password, 14),
+//         role: "user"
+//     }
+
+
+//     const result = await dbConnect(collections.USERS).insertOne(newUser)
+
+
+//     if(result.acknowledged){
+//         return {
+//             ...result, insertedId: result.insertedId.toString(),
+//     }}
+
+
+// }
+
+
+
+// export const loginUser= async(payload) => {
+//     const {email, password} = payload
+    
+//     if(!email || !password) return null
+    
+
+
+//     const user = await dbConnect(collections.USERS).findOne({email})
+
+
+//     if(!user) return null;
+
+//     const isMatched = await bcrypt.compare(password, user.password);
+//     if(isMatched){
+//         return user
+//     }
+//     else{
+//         return null
+//     }
+
+// }
+
+
+
+"use server";
+import { collections, dbConnect } from "@/lib/dbConnect";
+import bcrypt from "bcryptjs";
 
 export const postUser = async (payload) => {
+  const { email, password, name } = payload;
+  if (!email || !password) {
+    return {
+      success: false,
+    };
+  }
+  const isExist = await dbConnect(collections.USERS).findOne({ email });
+  if (isExist) {
+    return {
+      success: false,
+    };
+  }
+  const newUser = {
+    provider: "credentials",
+    name,
+    email,
+    password: await bcrypt.hash(password, 14),
+    role: "user",
+  };
 
+  const result = await dbConnect(collections.USERS).insertOne(newUser);
+  return {
+    ...result,
+    insertedId: result.insertedId?.toString(),
+  };
+};
 
-
-    const {email, password, name} = payload
-
-
-    if(!email || !password ) return null
-
-
-    const isExist = await dbConnect(collections.USERS).findOne({email})
-    if(isExist){
-        return null
-    }
-
-
-
-    const newUser = {
-        providerId: "credentials",
-        name, email, password: await bcrypt.hash(password, 14),
-        role: "user"
-    }
-
-
-    const result = await dbConnect(collections.USERS).insertOne(newUser)
-
-
-    if(result.acknowledged){
-        return {
-            ...result, insertedId: result.insertedId.toString(),
-    }}
-
-
-}
-
-
-
-export const loginUser= async(payload) => {
-    const {email, password} = payload
-    
-    if(!email || !password) return null
-    
-
-
-    const user = await dbConnect(collections.USERS).findOne({email})
-
-
-    if(!user) return null;
-
-    const isMatched = await bcrypt.compare(password, user.password);
-    if(isMatched){
-        return user
-    }
-    else{
-        return null
-    }
-
-}
-
+export const loginUser = async (payload) => {
+  const { email, password, name } = payload;
+  if (!email || !password) {
+    return null;
+  }
+  const user = await dbConnect(collections.USERS).findOne({ email });
+  if (!user) {
+    return null;
+  }
+  const isMatched = await bcrypt.compare(password, user?.password);
+  if (isMatched) {
+    return user;
+  }
+  return null;
+};
--- src/lib/authOptions.js
+++ src/lib/authOptions.js
@@ -1,78 +1,83 @@
-import CredentialsProvider from "next-auth/providers/credentials";
 import { loginUser } from "@/actions/server/auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { collections, dbConnect } from "./dbConnect";
-
 export const authOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
+
       credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
+        // username: { label: "Username", type: "text", placeholder: "jsmith" },
+        // password: { label: "Password", type: "password" },
       },
-      async authorize(credentials) {
-        const user = await loginUser(credentials);
-        if (!user) return null;
-        return {
-          id: user._id.toString(),
-          name: user.name,
-          email: user.email,
-          role: user.role,
-        };
+      async authorize(credentials, req) {
+        console.log(credentials);
+
+        const user = await loginUser({
+          email: credentials.email,
+          password: credentials.password,
+        });
+
+        // Return null if user data could not be retrieved
+        return user;
       },
     }),
-
-
     GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET
-  })
-
-
-
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log({ user, account, profile, email, credentials });
 
+      const isExist = await dbConnect(collections.USERS).findOne({
+        email: user.email,
+        // provider: account?.provider,
+      });
+      if (isExist) {
+        return true;
+      }
 
+      const newUser = {
+        provider: account?.provider,
+        email: user.email,
+        name: user.name,
+        image: user.image,
+        role: "user",
+      };
+      const result = await dbConnect(collections.USERS).insertOne(newUser);
 
-  
-callbacks: {
-  async signIn({ user, account, profile, email, credentials }) {
-
-
-    const isExist = await dbConnect(collections.USERS).find({email: user.email, provider:account?.provider,
-
-    });
-
-    if(isExist){
-      return true
-    }
-
-       const newUser = {
-            provider: account?.provider,
-            name:user.name,
-            email:user.email,
-            image:user.image, 
-            role: "user"
+      return result.acknowledged;
+      // return true
+    },
+    // async redirect({ url, baseUrl }) {
+    //   return baseUrl;
+    // },
+    async session({ session, token, user }) {
+      if (token) {
+        session.role = token?.role;
+        session.email = token?.email;
+      }
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      console.log("account data in token", account);
+      if (user) {
+        if (account.provider == "google") {
+          const dbUser = await dbConnect(collections.USERS).findOne({
+            email: user.email,
+          });
+          token.role = dbUser?.role;
+          token.email = dbUser?.email;
+        } else {
+          token.role = user?.role;
+          token.email = user?.email;
         }
-    
-        const result = await dbConnect(collections.USERS).insertOne(newUser);
-        return result.acknowledged
-
-
-
-    // return true
+      }
+      return token;
+    },
   },
-  // async redirect({ url, baseUrl }) {
-  //   return baseUrl
-  // },
-  // async session({ session, token, user }) {
-  //   return session
-  // },
-  // async jwt({ token, user, account, profile, isNewUser }) {
-  //   return token
-  // }
-}
 };
-
-
