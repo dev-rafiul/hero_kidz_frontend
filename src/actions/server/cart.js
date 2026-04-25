8111de4567229c0bb6666529885336ce896806f3
COMRADE040 <rafiulislam040@gmail.com>
2026-04-26 01:19:54 +0600

    cart button interactivity

--- src/actions/server/cart.js
+++ src/actions/server/cart.js
@@ -12,7 +12,52 @@ const cartCollection = dbConnect(collections.CART)
 
 export const handleCart = async ({product, inc = true}) => {
 
-    const user = await getServerSession(authOptions);
+    const user = await getServerSession(authOptions) || {};
+
+    if(!user?.user?.email) return {success: false}
+
+
+
+    const query = {
+        email: user?.email,
+        productId: product?._id
+    }
+
+    const isAdded = await cartCollection.findOne(query);
+
+    if(isAdded){
+        const updatedData = {
+            $inc: {
+                quantity: inc ? 1 : -1
+            }
+        }
+
+
+        const result = await cartCollection.updateOne(query, updatedData);
+        return {success: Boolean(result.modifiedCount)}
+
+
+    }else{
+
+        const newData = {
+            productId: product?._id,
+            email: user?.email,
+            title: product?.title,
+            quantity: 1,
+            image: product?.image,
+            price: product?.price - (product?.price * product?.discount) / 100,
+            username: user?.name,
+        }
+
+
+        const result = await cartCollection.insertOne(newData);
+        return {success: result.acknowledged}
+
+        
+    }
+
+
+
 
     return {success: true}
 
--- src/components/buttons/CartButton.jsx
+++ src/components/buttons/CartButton.jsx
@@ -2,7 +2,7 @@
 import { handleCart } from '@/actions/server/cart';
 import { useSession } from 'next-auth/react';
 import { usePathname, useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useState } from 'react';
 import { FaCartPlus } from 'react-icons/fa';
 import Swal from 'sweetalert2';
 
@@ -12,12 +12,14 @@ const CartButton = ({product}) => {
     const router = useRouter()
     const path = usePathname()
     const session = useSession();
+    const [isloading, setIsLoading] = useState(false);
     const isLogin = session?.status === "authenticated";
 
 
 
     
     const addtoCart = async () => {
+        setIsLoading(true);
         if(isLogin){
             const result = await handleCart({product, inc: true})
             if(result.success){
@@ -25,9 +27,11 @@ const CartButton = ({product}) => {
             }else{
                 Swal.fire("Oops", "Product not added to cart", "error")
             }
+            setIsLoading(false);
         }
             else{
         router.push(`/login?callbackUrl=${path}`)
+        setIsLoading(false);
         }
     }
 
@@ -35,7 +39,7 @@ const CartButton = ({product}) => {
 
     return (
         <div>
-            <button onClick={addtoCart} className="btn btn-primary btn-wide rounded-3xl flex gap-2">
+            <button disabled={session?.status == "loading" || isloading} onClick={addtoCart} className="btn btn-primary btn-wide rounded-3xl flex gap-2">
                       <FaCartPlus></FaCartPlus>
                       Add To Cart
                     </button>
